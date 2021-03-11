17da9dab8d6af6a2aa7bb9401584ed76515aa98f
monkey21254 <monkey21254@gmail.com>
2021-03-11 17:28:20 +0900

    Level 4 ~Step 43

--- Deep Learning/myPackage/functions.py
+++ Deep Learning/myPackage/functions.py
@@ -3,6 +3,14 @@ from myPackage.core_complex import Function
 from myPackage.core_complex import as_variable
 from myPackage import utils
 
+from myPackage import Variable # line 324 (get_array_module)
+gpu_enable = True
+try:
+    import cupy as cp
+    cupy = cp
+except ImportError:
+    gpu_enable = False
+
 
 class Sin(Function):
     def forward(self, x):
@@ -218,3 +226,115 @@ class MatMul(Function):
 
 def matmul(x, W):
     return MatMul()(x, W)
+
+
+class MeanSquaredError(Function):
+    """
+    MeanSquaredError Class
+
+    Methods
+    -------
+    forward : Calculate a loss(called y_hat).
+    backward : Perform back-prop using differentiation.
+    """    
+    def forward(self, x0, x1):
+        diff = x0 - x1
+        y = (diff ** 2).sum() / len(diff)
+        return y
+
+    def backward(self, gy):
+        x0, x1 = self.inputs
+        diff = x0 - x1
+        gx0 = gy * diff * (2. / len(diff))
+        gx1 = -gx0
+        return gx0, gx1
+
+
+def mean_squared_error(x0, x1):
+    return MeanSquaredError()(x0, x1)
+
+
+def mean_squared_error_simple(x0, x1):
+    x0, x1 = as_variable(x0), as_variable(x1)
+    diff = x0 - x1
+    y = sum(diff ** 2) / len(diff)
+    return y
+
+
+class Linear(Function):
+    """
+    Linear Class
+
+    Methods
+    -------
+    forward : Calculate a y means x*W(dot product).
+    backward : Perform back-prop from forward-prop equation (x*W + b = y).
+    """    
+    def forward(self, x, W, b):
+        y = x.dot(W)
+        if b is not None:
+            y += b
+        return y
+
+    def backward(self, gy):
+        x, W, b = self.inputs
+        gb = None if b.data is None else sum_to(gy, b.shape)
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+        return gx, gW, gb
+
+
+def linear(x, W, b=None):
+    return Linear()(x, W, b)
+
+
+def linear_simple(x, W, b=None):
+    t = matmul(x, W)
+    if b is None:
+        return t
+
+    y = t + b
+    t.data = None  # Release t.data (ndarray) for memory efficiency
+    return y
+
+
+class Sigmoid(Function):
+    def forward(self, x):
+        xp = get_array_module(x)
+        # y = 1 / (1 + xp.exp(-x))
+        y = xp.tanh(x * 0.5) * 0.5 + 0.5  # Better implementation
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * y * (1 - y)
+        return gx
+
+
+def sigmoid(x):
+    return Sigmoid()(x)
+
+
+def sigmoid_simple(x):
+    x = as_variable(x)
+    y = 1 / (1 + exp(-x))
+    return y
+
+
+def get_array_module(x): # line 6 (import ...)
+    """Returns the array module for `x`.
+    Args:
+        x (dezero.Variable or numpy.ndarray or cupy.ndarray): Values to
+            determine whether NumPy or CuPy should be used.
+    Returns:
+        module: `cupy` or `numpy` is returned based on the argument.
+    """
+    if isinstance(x, Variable):
+        x = x.data
+
+    if not gpu_enable:
+        return np
+    xp = cp.get_array_module(x)
+    return xp
+
+
--- Deep Learning/step/level4.py
+++ Deep Learning/step/level4.py
@@ -6,15 +6,66 @@ if '__file__' in globals():
 import numpy as np
 import myPackage.functions as F
 from myPackage import Variable
+import matplotlib.pyplot as plt
 
-x = Variable(np.array([[1, 2, 3], [4, 5, 6]]))
-y = F.sum(x, axis=0)
-y.backward()   # gy == variable([1 1 1])
-print(y)       # Variable([5 7 9])
-print(x.grad)  # Variable([[1 1 1], [1 1 1]])
+# x = Variable(np.array([[1, 2, 3], [4, 5, 6]]))
+# y = F.sum(x, axis=0)
+# y.backward()   # gy == variable([1 1 1])
+# print(y)       # Variable([5 7 9])
+# print(x.grad)  # Variable([[1 1 1], [1 1 1]])
 
-x = Variable(np.random.randn(2, 3, 4, 5))
-y = x.sum(keepdims=True)
-print(y.shape) # (1, 1, 1, 1)
+# x = Variable(np.random.randn(2, 3, 4, 5))
+# y = x.sum(keepdims=True)
+# print(y.shape) # (1, 1, 1, 1)
+
+
+# 데이터셋
+np.random.seed(0)
+x = np.random.rand(100, 1)
+y = np.sin(2 * np.pi * x) + np.random.rand(100, 1)
+
+# 가중치 초기화
+I, H, O = 1, 10, 1
+W1 = Variable(0.01 * np.random.randn(I, H))
+b1 = Variable(np.zeros(H))
+W2 = Variable(0.01 * np.random.randn(H, O))
+b2 = Variable(np.zeros(O))
+
+# 신경망 추론
+def predict(x):
+    y = F.linear(x, W1, b1)
+    y = F.sigmoid(y)
+    y = F.linear(y, W2, b2)
+    return y
+
+lr = 0.2
+iters = 10000
+
+# 신경망 학습
+for i in range(iters):
+    y_pred = predict(x)
+    loss = F.mean_squared_error(y, y_pred)
+
+    W1.cleargrad()
+    b1.cleargrad()
+    W2.cleargrad()
+    b2.cleargrad()
+    loss.backward()
+
+    W1.data -= lr * W1.grad.data
+    b1.data -= lr * b1.grad.data
+    W2.data -= lr * W2.grad.data
+    b2.data -= lr * b2.grad.data
+
+    if i % 1000 == 0:
+        print(loss)
+
+plt.scatter(x, y, s=10)
+plt.xlabel('x')
+plt.ylabel('y')
+t = np.arange(0, 1, .01)[:, np.newaxis]
+y_pred = predict(t)
+plt.plot(t, y_pred.data, color='r')
+plt.show()
 
 
