3a1ccf5ca626a4110d0b0271884eb54b797a17ad
monkey21254 <monkey21254@gmail.com>
2021-03-12 14:29:18 +0900

    ~Step 45

--- Deep Learning/Level4/level4_5.py
+++ Deep Learning/Level4/level4_5.py
@@ -2,3 +2,5 @@ if '__file__' in globals():
     import os, sys
     print('__file__ in globals at level4_5.py')
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+
--- Deep Learning/myPackage/models.py
+++ Deep Learning/myPackage/models.py
@@ -6,6 +6,8 @@ if '__file__' in globals():
 
 from myPackage import Layer
 from myPackage import utils
+import myPackage.functions as F
+import myPackage.layers as L
 
 
 class Model(Layer):
@@ -14,3 +16,19 @@ class Model(Layer):
         return utils.plot_dot_graph(y, verbose=True, to_file=to_file)
 
 
+class MLP(Model): # Multi-Layer Perceptron
+    def __init__(self, fc_output_sizes, activation=F.sigmoid):
+        super().__init__()
+        self.activation = activation
+        self.layers = []
+
+        for i, out_size in enumerate(fc_output_sizes):
+            layer = L.Linear(out_size)
+            setattr(self, 'l' + str(i), layer)
+            self.layers.append(layer)
+
+    def forward(self, x):
+        for l in self.layers[:-1]:
+            x = self.activation(l(x))
+        return self.layers[-1](x)
+    
